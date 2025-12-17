55d2ea4682b73b10f4eac9857aa62541db564366
Zach Maddox <zmad5306@protonmail.com>
2025-12-17 09:51:52 -0500

    fixes

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,21 @@
 plugins {
-
+  id("com.gradleup.nmcp.aggregation")
 }
 
 group = "dev.zachmaddox.compose"
 version = "1.1.0"
+
+nmcpAggregation {
+  centralPortal {
+    username = System.getenv("OSSRH_USERNAME")
+      ?: error("Missing OSSRH_USERNAME (Central Portal token username)")
+    password = System.getenv("OSSRH_PASSWORD")
+      ?: error("Missing OSSRH_PASSWORD (Central Portal token password)")
+
+    publishingType = "AUTOMATIC"
+  }
+}
+
+dependencies {
+  nmcpAggregation(project(":compose-reorderable-grid"))
+}
